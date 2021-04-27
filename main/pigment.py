1116f3f73ed91a8ebe91c93784dd4b7ee974386c
mreppert <reppertm@control.nanohub.org>
2021-04-27 10:19:37 -0400

    updated main

---
+++ main/pigment.py
@@ -0,0 +1,196 @@
+# class category is used to classify input molecules into broad groups. 
+# Each instance of a category includes two traits:
+#  * category.name: String used to identify the class
+#
+#  * category.BaseNames: List of atom names (strings) that *must*
+#       be identified in the input molecule if it is to be considered a 
+#       member of this category
+#
+# atom names that *must* be present for a molecule to be considered 
+class category:
+    def __init__(self, name, BaseNames):
+        self.name = name
+        self.BaseNames = BaseNames
+
+# class species define a specific chemical identities for different 
+# pigments, e.g., chlorophyll b or pheophytin a. The class traits include:
+# 
+# * pclass: The pigment.category to which the species belongs
+# 
+# * name: The (not necessarily unique) residue name commonly used 
+#      to refer to the pigment, e.g., "Chl b" for Chlorophyll a  or "Pheo a" for
+#      pheophytin a. 
+# 
+# * stdname: A unique, standardized residue name abbreviation used 
+#      for internal referencing within PigmentHunter. E.g., CLB for Chlorophyll b
+#      or PHA for pheophytin a. 
+# 
+# * xnames: A list of atom names used to define the pigment species. This list
+#      should not include atoms on the BaseNames list used to define the
+#      pigment.category, although different species may hold xnames in common. 
+#
+class species:
+    def __init__(self, ResName, StdName, pClass, XAtNames, diplength):
+        self.pclass = pClass,
+        self.name = ResName
+        self.stdname = StdName
+        self.xnames = XAtNames
+        self.diplength = diplength
+        
+        
+# Instances of class pigment store data for a particular pigment molecule 
+# described by imported structural data (e.g., from a PDB file). Its
+# traits include:
+# * idx: The residue index for this molecule in the parmed.struc
+#     from which it was extracted. Thus struc.residues[idx] returns
+#     the corresponding parmed residue object. 
+# 
+# * species: the instance of the pigment.species class associated with this pigment
+#     (e.g., representing CLA, CLB, PHA, etc.)
+# 
+# * alist: A list of "allowed" species that *could* be associated
+#       with this pigment, based solely on the structural date present 
+#       in the parmed.struc from which it was extracted. Many pigment types
+#       cannot be identified uniquely from heavy atoms only (particularly since
+#       the structure could be incomplete); alist enumerates all recognized 
+#       species consistent with the available structural data. 
+#
+# * residue: the parmed residue object corresponding to this pigment
+# 
+# * atnames: a list of atom name strings for each atom in the 
+#       corresponding parmed residue
+# 
+# * atcoords: a coordinates matrix for the atoms named in atnames
+#
+class pigment:
+    def __init__(self, idx, spec, alist, residue, atnames, atcoords):
+        self.idx = idx
+        self.species = spec
+        self.alist = alist
+        self.atnames = atnames
+        self.atcoords = atcoords
+        self.widget = []
+        self.residue = residue
+
+# Returns a list of all residue indices (in struc)
+# that contain all required atoms
+def find_pigments(NameList, instruc):
+
+    SelStr = "(:0-"+str(len(instruc.residues)) + ")"
+    for ndx in range(1, len(NameList)):
+        tstruc = instruc.view[SelStr + ' & (@'+NameList[ndx] + ")"]
+        SelNdx = []
+        for at in tstruc:
+            SelNdx.append(at.residue.idx)
+        SelStr = "(:" + ",".join([str(i+1) for i in SelNdx]) + ")"
+        if len(SelNdx)==0:
+            break
+
+    return SelNdx
+    
+# Now check which pigment types can be *excluded* based on the atoms
+# that are present in each pigment. NB: We do *not* require all atoms
+# to be present. 
+# The return argument AList is a list of pigment types that *are* allowed. 
+def eliminate_types(PigNdcs, PClass, TypeList, UNK, instruc):
+    
+    AList = []
+    for ndx in PigNdcs:
+        tstruc = instruc[':'+str(ndx+1)]
+        
+        # For each atom in the pigment, check whether it's allowed
+        # under each pigment type. 
+        TypeAllowed = [1 for i in range(0,len(TypeList))]
+        for at in tstruc:
+            # If not a Hydrogen...we always ignore hydrogens
+            if at.name[0]!='H':
+                # First check if it's a base atom. If not, we check 
+                # the characteristic atoms for each pigment. 
+                if (PClass.BaseNames.count(at.name)==0):
+                    for tndx in range(0, len(TypeList)):
+                        if TypeList[tndx].xnames.count(at.name)==0:
+                            TypeAllowed[tndx] = 0
+        
+        alist = []
+        for tndx in range(0, len(TypeList)):
+            if TypeAllowed[tndx]:
+                alist.append(TypeList[tndx])
+                
+        if len(alist)==0:
+            alist = [UNK]
+        
+        AList.append(alist)
+        
+    return AList
+
+
+# MList[p] is a list of matched types for pigment p. Here a "match"
+# means that *all* the species.xnames atoms are present
+# in the pigments parmed.residue.
+def match_types(PigNdcs, AList, instruc):
+    
+    MList = []
+    # Loop through pigments
+    for p in range(0, len(PigNdcs)):
+        ndx = PigNdcs[p]
+        
+        tstruc = instruc[':'+str(ndx+1)]
+        
+        # alist is a list of pigment types allowed for pigment p
+        alist = AList[p]
+        
+        # mlist will store a list of types that match the pigment exactly
+        mlist = []
+        
+        # Loop through possible types
+        for t in range(0, len(alist)):
+            
+            # Start assuming the pigment matches the type
+            match = True
+            typ = alist[t]
+            for name in typ.xnames:
+                
+                # If any atom in type.xnames is NOT found in the corresponding
+                # instruc.residue set match to False and break
+                if len(tstruc['@'+name].atoms)==0:
+                    match = False
+                    break
+                
+            # If we have a match, append it to the pigment's match list
+            if match:
+                mlist.append(typ)
+                
+        MList.append(mlist)
+    return MList
+
+# 1. Assign all pigments to the top entry in MList[p] if non-empty
+# 2. For each pigment still unassigned, check whether any pigments with the 
+#    same residue.name have already been assigned. If so, assign to the same type. 
+# 3. For each pigment still unassigned, assign to the top entry in AList[p] if non-empty
+def assign_pigments(PigNdcs, MList, AList, instruc):
+    TList = []
+    
+    # 1. Assign by MList
+    for p in range(0, len(PigNdcs)):
+        if len(MList[p])>0:
+            TList.append(MList[p][0])
+        else:
+            TList.append([])
+            
+    # 2. Assign by name
+    for p in range(0, len(PigNdcs)):
+        if TList[p]==[]:
+            rname = instruc.residues[PigNdcs[p]].name
+            
+            # Check if any assigned pigment has a matching name 
+            for pref in range(0, len(PigNdcs)):
+                if (TList[pref]!=[]) and (rname==instruc.residues[PigNdcs[pref]].name):
+                    TList[p] = TList[pref]
+                    break
+                    
+    # 3. If pigments are still unmatched, assign to top alist entry
+    for p in range(0, len(PigNdcs)):
+        if TList[p]==[]:
+            TList[p] = AList[p][0]
+                
+    return TList
---
+++ main/tresp.py
@@ -0,0 +1,322 @@
+import numpy as np
+import parmed as pmd
+
+import os,sys
+sys.path.append('./main/')
+
+import pigment
+
+def calculate_coupling(PigList, ChainList):
+    
+    # First create list of chain-selected pigments
+    SelPigs = []
+    for pig in PigList:
+        if ChainList.count(pig.residue.chain)>0:
+            SelPigs.append(pig)
+    
+    Npigs = len(SelPigs)
+    CoupMat = np.zeros((Npigs,Npigs))
+    Dips = []
+    h = 6.62607015e-34 #J*s
+    c = 2.998e10 #cm/s
+    eo = 4.80320451e-10 # esu
+    
+    Erg2J = 1.0e-7
+    ang2cm = 1.0e-8
+    
+    # Now check whether TrESP parameters are available for *all*
+    # selected pigments and (if so) store them for reference.
+    ListAtoms = []
+    ListQ00 = []
+    ListQ10 = []
+    ListQ11 = []
+    error = False
+    
+    # Loop over pigments
+    for p in range(0, Npigs):
+        pig = SelPigs[p]
+        
+        # TrESP parameters should be in file 'misc/TrESP/' + pig.species.stdname + '.txt'
+        fname = 'misc/TrESP/' + pig.species.stdname + '.txt'
+        if os.path.isfile(fname)==False:
+            print('Error: Could not locate TrESP parameter file for species ' + pig.species.stdname)
+            error = True
+            break
+        else:
+            atoms=  []
+            q00 = []
+            q10 = []
+            q11 = []
+            with open(fname) as fd:
+                for line in fd:
+                    if len(line)>0 and line[0]!="#":
+                        items = line.split()
+                        format_error = False
+                        try:
+                            atoms.append(items[0])
+                            
+                            # TrESP file charges are in units of 1000*eo.
+                            # We convert here to eo. 
+                            q00.append(float(items[1])*1.0e-3)
+                            q10.append(float(items[2])*1.0e-3)
+                            q11.append(float(items[3])*1.0e-3)
+                            
+                        except:
+                            print('Error reading TrESP input file ' + fname + '.')
+                            print('Aborting TrESP calculation.')
+                            error = True
+                            break
+                            
+            ListAtoms.append(atoms)
+            ListQ00.append(q00)
+            ListQ10.append(q10)
+            ListQ11.append(q11)
+            
+        if(error):
+            break
+                            
+    # If no errors, we've located all relevant TrESP files and imported the data. 
+    # Now check whether all necessary atoms are available in the structure.
+    if error==False:
+        for p in range(0, Npigs):
+            # Loop over TrESP atoms
+            for n in range(0, len(ListAtoms[p])):
+
+                # name for the nth TrESP atom in pth pigment
+                name = ListAtoms[p][n]
+
+                # If we can't find it in the structure, throw an error. 
+                # pig.atnames stores the names of all atoms associated with pigment pig
+                if pig.atnames.count(name)==0:
+                    print('Error: Could not locate TrESP atom ' + name + " in pigment " + pig.residue.name + " " + pig.residue.chain + " " + str(pig.residue.number))
+                    print('Aborting TrESP calculation.')
+                    error = True
+                    break
+
+            # If there's already been an error, don't continue. 
+            if error:
+                break
+                
+    # If no errors, calculate centers
+    CentMat = np.zeros((Npigs,3))
+    CentAtoms = ['NA', 'NB', 'NC', 'ND']
+    for p in range(0, Npigs):
+        pig = SelPigs[p]
+        for name in CentAtoms:
+            if pig.atnames.count(name)==0:
+                print('Error: Could not locate center atom ' + name + " in pigment " + pig.residue.name + " " + pig.residue.chain + " " + str(pig.residue.number))
+                error = True
+                print('Aborting TrESP calculation.')
+                break
+            else:
+                ndx = pig.atnames.index(name)
+                CentMat[p,:] += pig.atcoords[ndx]/float(len(CentAtoms))
+
+#     for p in range(0, Npigs):
+#         pig = SelPigs[p]
+#         MGndx = pig.atnames.index('MG')
+#         print('Calculated: ' + str(CentMat[p]))
+#         print('MG: ' + str(pig.atcoords[MGndx]))
+#         print('')
+    
+    # If no errors, all pigments have TrESP parameters and necessary atoms, 
+    # and pigment centers have been calculated and stored in CentMat.
+    if error==False:
+        # Build list of TrESP coordinates for each pigment
+        # NB: Units are converted to cm! (cgs units)
+        CoordList = []
+        for p in range(0, Npigs):
+            pig = SelPigs[p]
+            coords = []
+            # Find index associated with each TrESP atom in the structure.
+            # Coordinate list will be sorted in atom order specified in TrESP 
+            # parameter file *not* order of crystal structure. 
+            for name in ListAtoms[p]:
+                ndx = pig.atnames.index(name)
+                coords.append(pig.atcoords[ndx])
+            CoordList.append(np.array(coords)*ang2cm)
+            
+        # Calculate pigment dipole lengths (as per default TrESP charges)
+        # We'll divide charges for each pigment by the ratio of the *calculated*
+        # dipole length to the *standard* dipole length. 
+        # Charges are in units of cm^3/2 g^1/2 s^−1.
+        DipLengths = []
+        for p in range(0, Npigs):
+            pig = SelPigs[p]
+            dip = np.zeros((3,))
+            for n in range(0, len(ListAtoms[p])):
+                dip += eo*ListQ10[p][n]*CoordList[p][n]
+                
+            DipLengths.append(np.linalg.norm(dip))
+            Dips.append(dip/np.linalg.norm(dip))
+        Dips = np.array(Dips)
+        
+        # Calculate interactions
+        for p1 in range(0, Npigs):
+            pig1 = SelPigs[p1]
+            df1 = pig1.species.diplength/DipLengths[p1]
+            for p2 in range(0, p1):
+                pig2 = SelPigs[p2]
+                df2 = pig2.species.diplength/DipLengths[p2]
+                for atm in range(0, len(ListAtoms[p1])):
+                    for atn in range(0, len(ListAtoms[p2])):
+                        Rmn = CoordList[p1][atm,:] - CoordList[p2][atn,:]
+                        rmn = np.linalg.norm(Rmn)
+                        CoupMat[p1,p2] += eo*eo*df1*df2*(ListQ10[p1][atm]*ListQ10[p2][atn]/rmn)*(Erg2J/h)/c
+                        
+            
+        CoupMat = CoupMat + np.transpose(CoupMat)
+        
+        # Calculate rotation matrix
+        RotMat = np.zeros((Npigs,Npigs))
+        for m in range(0, Npigs):
+            for n in range(0, Npigs):
+                Rmn = CentMat[n,:] - CentMat[m,:]
+                #osc = SelPigs[m].species.diplength*SelPigs[n].species.diplength
+                osc = 1.0
+                RotMat[m,n] = np.dot(Rmn, np.cross(Dips[m,:], Dips[n,:]))*osc
+        
+    # NB: oscillator strengths are all finally scaled to unity!
+    return CoupMat, Dips, RotMat
+
+# Picks the residue to which the atom closest to MG belongs
+def locate_ligand(pig, instruc):
+    # Only do this if an MG atom is listed in the structure
+    if pig.atnames.count('MG')!=0:
+        MGndx = pig.atnames.index('MG')
+        data_frame = instruc.to_dataframe()
+        bgndcs = (data_frame.resid != pig.residue.idx)
+        dvec = np.sqrt(np.sum(np.power(pig.atcoords[MGndx] - instruc.coordinates[bgndcs],2),1))
+        ndx0 = np.argmin(dvec)
+        at0 = instruc[bgndcs].atoms[ndx0]
+        return at0.residue
+    else:
+        return []
+
+
+def calculate_shift(PigList, ChainList, instruc):
+    
+    # First create list of chain-selected pigments
+    SelPigs = []
+    for pig in PigList:
+        if ChainList.count(pig.residue.chain)>0:
+            SelPigs.append(pig)
+    
+    Npigs = len(SelPigs)
+    tFreqs = np.zeros((Npigs,))
+        
+    h = 6.62607015e-34 #J*s
+    c = 2.998e10 #cm/s
+    eo = 4.80320451e-10 # esu
+    eps_eff = 2.5
+    
+    Erg2J = 1.0e-7
+    ang2cm = 1.0e-8
+    
+    # Now check whether TrESP parameters are available for *all*
+    # selected pigments and (if so) store them for reference.
+    ListAtoms = []
+    ListQ00 = []
+    ListQ10 = []
+    ListQ11 = []
+    error = False
+    
+    # Loop over pigments
+    for p in range(0, Npigs):
+        pig = SelPigs[p]
+        
+        # TrESP parameters should be in file 'misc/TrESP/' + pig.species.stdname + '.txt'
+        fname = 'misc/TrESP/' + pig.species.stdname + '.txt'
+        if os.path.isfile(fname)==False:
+            print('Error: Could not locate TrESP parameter file for species ' + pig.species.stdname)
+            error = True
+            break
+        else:
+            atoms=  []
+            q00 = []
+            q10 = []
+            q11 = []
+            with open(fname) as fd:
+                for line in fd:
+                    if len(line)>0 and line[0]!="#":
+                        items = line.split()
+                        format_error = False
+                        try:
+                            atoms.append(items[0])
+                            q00.append(float(items[1]))
+                            q10.append(float(items[2]))
+                            q11.append(float(items[3]))
+                        except:
+                            print('Error reading TrESP input file ' + fname + '.')
+                            print('Aborting TrESP calculation.')
+                            error = True
+                            break
+                            
+            ListAtoms.append(atoms)
+            ListQ00.append(q00)
+            ListQ10.append(q10)
+            ListQ11.append(q11)
+            
+        if(error):
+            break
+                            
+    # If no errors, we've located all relevant TrESP files and imported the data. 
+    # Now check whether all necessary atoms are available in the structure.
+    if error==False:
+        for p in range(0, Npigs):
+            # Loop over TrESP atoms
+            for n in range(0, len(ListAtoms[p])):
+
+                # name for the nth TrESP atom in pth pigment
+                name = ListAtoms[p][n]
+
+                # If we can't find it in the structure, throw an error. 
+                # pig.atnames stores the names of all atoms associated with pigment pig
+                if pig.atnames.count(name)==0:
+                    print('Error: Could not locate TrESP atom ' + name + " in pigment " + pig.residue.name + " " + pig.residue.chain + " " + str(pig.residue.number))
+                    print('Aborting TrESP calculation.')
+                    error = True
+                    break
+
+            # If there's already been an error, don't continue. 
+            if error:
+                break
+                
+    # If no errors, all pigments have TrESP parameters and necessary atoms. 
+    if error==False:
+        # Build list of TrESP coordinates for each pigment
+        # NB: Units are converted to cm! (cgs units)
+        CoordList = []
+        for p in range(0, Npigs):
+            pig = SelPigs[p]
+            coords = []
+            # Find index associated with each TrESP atom in the structure.
+            # Coordinate list will be sorted in atom order specified in TrESP
+            # parameter file *not* order of crystal structure. 
+            for name in ListAtoms[p]:
+                ndx = pig.atnames.index(name)
+                coords.append(pig.atcoords[ndx])
+            CoordList.append(np.array(coords)*ang2cm)
+            
+        
+        for p in range(0, Npigs):
+            
+            pig = SelPigs[p]
+            shift = 0.0
+            
+            #lig = locate_ligand(pig, instruc)
+            data_frame = instruc.to_dataframe()
+            
+            # We exclude the pigment itself from calculation
+            #ndcs = np.logical_and(data_frame.resid != pig.res.idx, data_frame.resid != lig.idx)
+            ndcs = data_frame.resid != pig.residue.idx
+            
+            for atm in range(0, len(ListAtoms[p])):
+                dQ = ListQ11[p][atm] - ListQ00[p][atm]
+                Rvec = (CoordList[p][atm,:] - instruc.coordinates[ndcs,:]*ang2cm)
+                Dvec = np.sqrt(np.sum(np.power(Rvec,2),1))
+                Qvec = data_frame[ndcs].charge
+                shift += eo*eo*dQ*np.sum(np.divide(Qvec,Dvec))*Erg2J/(h*c*eps_eff)
+            tFreqs[p] = shift
+        
+    return tFreqs, error
