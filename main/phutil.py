57e10edfac9a4df75190390ba766d79c3b21d997
mreppert <reppertm@12896cc98c6a>
2023-06-01 18:09:40 +0000

    major update: fluorescence + line shape

--- main/phutil.py
+++ main/phutil.py
@@ -1,21 +1,16 @@
-import os,sys
-import subprocess
+import os
 
+# Build data folder structure if necessary. 
 def build_data_folders(DATADIR):
-    return
     
-#     if os.path.isdir(DATADIR)==0:
-#         subprocess.run(['mkdir', '-p', DATADIR])
+    # Check if DATADIR exists
+    if os.path.isdir(DATADIR)==0:
+        # If not, create it. 
+        subprocess.run(['mkdir', '-p', DATADIR])
 
-#         # Check that necessary subdirectories exist
-#         subdirs = ['calc', 'exc', 'md', 'pdb', 'spec']
-#         for fold in subdirs:
-#             if os.path.isdir(DATADIR + "/" + fold)==0:
-#                 subprocess.run(['mkdir', '-p', DATADIR + '/' + fold])
-
+        # Now create any necessary subdirectories
+        subdirs = ['calc', 'exc', 'md', 'pdb', 'spec']
+        for fold in subdirs:
+            if os.path.isdir(DATADIR + "/" + fold)==0:
+                subprocess.run(['mkdir', '-p', DATADIR + '/' + fold])
     return
-
-def get_data_dir():
-    DATADIR = os.environ['DATADIR']
-    return os.path.relpath(DATADIR)
-
